864082032f7776f4b2480dbe3214e8e7ff5d7e76
kuclimber <everlastingomega@gmail.com>
2017-06-23 13:17:22 -0700

    take2

--- index.php
+++ index.php
@@ -3,9 +3,15 @@
   <title>PHP Test</title>
  </head>
  <body>
+ <script>
+ 	var dpm = function(){
+ 		alert('Don\'t push me cause i\'m close to the edge');
+ 	}
+ </script>
  <p> this is a test</p>
  <?php echo '<p>Hello World!!!</p>'; ?> 
  <?php echo '<p>LA LA LA</p>'; ?> 
- <button onclick="function(){alert('Don\'t push me cause i\'m close to the edge')}">Push Me</button>
+ <button onclick="dpm()">Push Me</button>
+
  </body>
 </html>
